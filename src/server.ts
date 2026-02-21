fdedd29d76e273a75b4384832723c36605046f10
loki0b <contato@henriquegarcialeite.com>
2026-02-21 11:05:58 -0300

    (feat): add server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { json } from "express";
+import cookieParser from "cookie-parser";
+import userRoutes from "@routes/userRoutes"
+
+import type { Express } from "express";
+
+const app: Express = express();
+const port: string | number = process.env["PORT"] || 8080;
+
+
+app.use(json());
+app.use(cookieParser());
+app.use("/api/user/", userRoutes);
+
+app.listen(port, () => {
+    console.log(`Service running: http://localhost:${port}/`)
+})
