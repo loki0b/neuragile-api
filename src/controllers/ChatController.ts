abb12db68ad8a6765c96a1eece9796190d24e7a9
loki0b <contato@henriquegarcialeite.com>
2026-03-26 07:49:38 -0300

    (feat): add chat controller

---
+++ src/controllers/ChatController.ts
@@ -0,0 +1,29 @@
+import { GoogleGenAI } from "@google/genai";
+import { systemInstruction } from "src/lib/systemInstruction.js";
+
+class ChatController {
+    genAI = new GoogleGenAI({});
+
+    async sendMessage(req: any, res: any, next: any) {
+        const { userMessage } = req.body;
+
+        try {
+            const response = await this.genAI.models.generateContent({
+                model: "gemini-3-flash-preview",
+                config: {
+                    systemInstruction: systemInstruction,
+                    responseMimeType: "application/json"
+                },
+                contents: userMessage
+            });
+
+            return res.status(200).json(JSON.parse(String(response.text)));
+        } catch (err) {
+            console.log(err);
+
+            return res.status(500).json({"status": "Server Error"});
+        }
+    }
+}
+
+export default ChatController;
